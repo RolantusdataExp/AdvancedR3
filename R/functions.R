8ad6ab9fa5627048fc68f389be6719c9503983a4
Peter Roland Jakobsgaard <pete@sdu.dk>
2023-11-09 15:38:10 +0100

    Recipe creation

--- R/functions.R
+++ R/functions.R
@@ -25,3 +25,47 @@ plot_distributions = function(data) {
     data %>%
         ggplot2::ggplot( ggplot2::aes(x = value)) + ggplot2::geom_histogram() +            ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
 }
+
+#' Change to snakecase a given column!
+#'
+#' @param dataframe
+#' @param cols - coloumn in dataframe
+#'
+#' @return
+#'
+#'
+#' @examples
+column_values_to_snake_case <- function(data, cols) {
+    data %>%
+        dplyr::mutate(dplyr::across({{ cols }}, snakecase::to_snake_case))
+}
+
+#' Function to create a pivote-wider dataformat
+#'
+#' @param data
+#'
+#' @return pivot-wider
+#' @export
+#'
+#' @examples
+metabolites_to_wider = function(data) {
+    data %>%
+        pivot_wider(names_from = metabolite, values_from = value, values_fn = mean, names_prefix = "metabolite_")
+}
+
+#' A transformation recipe to pre-process the data.
+#'
+#' @param data The lipidomics dataset.
+#' @param metabolite_variable The column of the metabolite variable.
+#'
+#' @return
+#'
+create_recipe_spec <- function(data, metabolite_variable) {
+    recipes::recipe(data) %>%
+        recipes::update_role({{ metabolite_variable }}, age, gender, new_role = "predictor") %>%
+        recipes::update_role(class, new_role = "outcome") %>%
+        recipes::step_normalize(tidyselect::starts_with("metabolite_"))
+}
+
+
+
